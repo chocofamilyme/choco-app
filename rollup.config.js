5bdbd950972b61ebe5d62f64acd8606bb6eb0ad7
Dastan Abdraym <dastan.abdraym@mail.ru>
2022-11-08 12:30:04 +0600

    typescript added

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+const input = ['src/index.ts'];
+
+export default {
+    input,
+    plugins: [
+        typescript(),
+        nodeResolve({ browser: true }),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled'
+        }),
+        terser()
+    ],
+    output: {
+        file: `dist/${pkg.name}.min.js`,
+        format: 'umd',
+        name: 'rahmetAuth',
+        esModule: false,
+        exports: 'named',
+        sourcemap: true
+    }
+};
