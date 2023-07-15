cccd85f25d851008bcf0759bdbb53a750fc1f6c9
Dastan Abdraym <dastan.abdraym@mail.ru>
2023-07-15 12:50:25 +0600

    add support for cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,73 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
 
 const input = ['src/index.ts'];
 
-export default {
-    input,
-    plugins: [
-        typescript({ tsconfig: './tsconfig.json' }),
-        nodeResolve({ browser: true }),
-        commonjs(),
-        babel({
-            exclude: 'node_modules/**',
-            babelHelpers: 'bundled'
-        }),
-        terser()
-    ],
-    output: {
-        file: `dist/index.js`,
-        format: 'umd',
-        name: 'rahmetAuth',
-        esModule: false,
-        exports: 'named',
-        sourcemap: true
+export default [
+    // umd
+    {
+        input,
+        plugins: [
+            typescript({
+                compilerOptions: {
+                    declaration: false
+                }
+            }),
+            nodeResolve({ browser: true }),
+            babel({
+                exclude: 'node_modules/**',
+                babelHelpers: 'bundled'
+            }),
+            terser()
+        ],
+        output: {
+            file: `dist/index.min.js`,
+            format: 'umd',
+            name: 'chocoApp',
+            esModule: false,
+            exports: 'named',
+            sourcemap: true
+        }
+    },
+
+    // cjs
+    {
+        input,
+        plugins: [
+            typescript({
+                compilerOptions: {
+                    outDir: 'dist/cjs',
+                    declarationDir: 'dist/cjs'
+                }
+            }),
+            nodeResolve()
+        ],
+        output: {
+            dir: 'dist/cjs',
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true
+        }
+    },
+
+    // esm
+    {
+        input,
+        plugins: [
+            typescript({
+                compilerOptions: {
+                    outDir: 'dist/esm',
+                    declarationDir: 'dist/esm'
+                }
+            }),
+            nodeResolve()
+        ],
+        output: {
+            dir: 'dist/esm',
+            format: 'esm',
+            exports: 'named',
+            sourcemap: true
+        }
     }
-};
+];
