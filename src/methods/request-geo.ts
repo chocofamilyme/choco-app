ce71f137df46864e721cfefa3a1aa5946aa51235
Dastan Abdraym <dastan.abdraym@mail.ru>
2023-07-31 12:25:36 +0600

    fix

--- src/methods/request-geo.ts
+++ src/methods/request-geo.ts
@@ -1,5 +1,5 @@
 import { connect } from '../utils';
-import { GeoStatus } from '../ts/enum/geo-status.enum';
+import { GeoStatus } from '../ts/enum';
 
 export const requestGeoPermissionStatus = (): Promise<GeoStatus> => {
     return connect((resolve: (status: GeoStatus) => GeoStatus) => {
