85c8b332aab5afaf7610a9823555651f9906acfb
Dastan Abdraym <dastan.abdiraiym@bd.qatarinsurance.com>
2023-09-28 15:12:44 +0600

    значение по умолчанию для notification

--- src/methods/notification.ts
+++ src/methods/notification.ts
@@ -5,9 +5,10 @@ export const getNotificationStatus = (): Promise<NotificationStatus> => {
     return connect((resolve: (status: NotificationStatus) => NotificationStatus) => {
         if (typeof window.RahmetApp.getNotificationStatus === 'function') {
             window.RahmetApp.getNotificationStatus();
+            window.RahmetWebApp.didGetIsNotificationStatus = (status: NotificationStatus) => resolve(status);
+        } else {
+            resolve(NotificationStatus.notDetermined);
         }
-
-        window.RahmetWebApp.didGetIsNotificationStatus = (status: NotificationStatus) => resolve(status);
     });
 };
 
@@ -15,8 +16,9 @@ export const requestNotificationStatus = (): Promise<NotificationStatus> => {
     return connect((resolve: (status: NotificationStatus) => NotificationStatus) => {
         if (typeof window.RahmetApp.requestNotificationStatus === 'function') {
             window.RahmetApp.requestNotificationStatus();
+            window.RahmetWebApp.didGetIsNotificationStatus = (status: NotificationStatus) => resolve(status);
+        } else {
+            resolve(NotificationStatus.notDetermined);
         }
-
-        window.RahmetWebApp.didGetIsNotificationStatus = (status: NotificationStatus) => resolve(status);
     });
 };
