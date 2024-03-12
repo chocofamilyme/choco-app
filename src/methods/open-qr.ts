bca356ad5d916fa857215813cd37da5a67733805
Asatulla <debixxter@gmail.com>
2024-03-12 12:04:34 +0500

    [EX-5206] fix: Баг в методе openQRScannerWithTextResult

--- src/methods/open-qr.ts
+++ src/methods/open-qr.ts
@@ -2,6 +2,10 @@ import { connect } from '../utils';
 
 export const openQR = (): Promise<string> => {
     return connect((resolve: (text: string) => string) => {
+        if (!window.RahmetApp.openQRScannerWithTextResult) {
+            return resolve('');
+        }
+
         window.RahmetApp.openQRScannerWithTextResult();
         window.RahmetWebApp.didScanQrText = text => resolve(text);
     });
